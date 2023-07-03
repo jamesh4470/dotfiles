028840ed5d2477219f09c47cfd85d1adb2ea2a28
Cheesenthusiast <111305449+Cheesenthusiast@users.noreply.github.com>
2023-07-02 21:50:50 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,7 +105,7 @@ return require('packer').startup(function(use)
             { name = 'ultisnips' }
         }, {
             { name = 'buffer' }
-	    })
+	})
     })
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
