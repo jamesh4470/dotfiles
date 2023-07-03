345b0191729b61acfb96ae11edc3a6c424b3b520
Cheesenthusiast <111305449+Cheesenthusiast@users.noreply.github.com>
2023-07-03 09:11:16 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,115 +8,175 @@ vim.opt.signcolumn = "yes"
 vim.bo.softtabstop = 4
 vim.wo.number = true
 
-return require('packer').startup(function(use)
-
-    use {"wbthomason/packer.nvim"}
-    use {"navarasu/onedark.nvim"}
-    use {'nvim-lualine/lualine.nvim'}
-    use {"nvim-treesitter/nvim-treesitter"}
-    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-        require("toggleterm").setup()
-    end}
-    use {"williamboman/mason.nvim"}
-    use {"windwp/nvim-autopairs"}
-    use {"nvim-tree/nvim-tree.lua"}
-    use {"andweeb/presence.nvim"}
-    use {"nvim-tree/nvim-web-devicons"}
-    use {"lukas-reineke/indent-blankline.nvim"}
-    use {"lewis6991/gitsigns.nvim"}
-    use {"romgrk/barbar.nvim"}
-    use {"numToStr/Comment.nvim"}
-
-    use {"neovim/nvim-lspconfig"}
-    use {"hrsh7th/nvim-cmp"}
-    use {"hrsh7th/cmp-nvim-lsp"}
-    use {"SirVer/ultisnips"}
-    use {"quangnguyen30192/cmp-nvim-ultisnips"}
-
-    require("onedark").setup({style = "darker"})
-    require("onedark").load()
-
-    require("lualine").setup()
-
-    require "nvim-treesitter.configs".setup({
-        highlight = {
-            enable = true
-        }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
     })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    require("toggleterm").setup ({
-        open_mapping = [[<c-\>]],
-        direction = "float",
-        float_opts = {
-            border = "curved"
-        }
-    })
+require("lazy").setup({
+    {
+        "navarasu/onedark.nvim",
+        config = function()
+            require("onedark").setup({style = "darker"})
+            require("onedark").load()
+        end
+    },
 
-    require("mason").setup()
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("lualine").setup()
+        end
+    },
 
-    require("nvim-autopairs").setup()
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require "nvim-treesitter.configs".setup({
+                highlight = {
+                    enable = true
+                }
+            })
+        end
+    },
 
-    require("nvim-tree").setup({
-        vim.keymap.set("n", "<C-t>", require("nvim-tree.api").tree.toggle),
-        renderer = {
-            indent_markers = {
-                enable = true
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]],
+                direction = "float",
+                float_opts = {
+                    border = "curved"
+                }
+            })
+        end
+    },
+
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup()
+        end
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = function()
+            require("nvim-tree").setup({
+                vim.keymap.set("n", "<C-t>", require("nvim-tree.api").tree.toggle),
+                renderer = {
+                    indent_markers = {
+                        enable = true
+                    }
+                }
+            })
+        end
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("indent_blankline").setup({
+                show_current_context = true
+            })
+        end
+    },
+
+    {
+        "romgrk/barbar.nvim",
+        config = function()
+            require("barbar").setup({
+                animations = true
+            })
+        end
+    },
+
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup({
+                toggler = {
+                    line = [[<C-_>]]
+                },
+                opleader = {
+                    line = [[<C-_>]]
+                }
+            })
+        end
+    },
+
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require("alpha").setup(require("alpha.themes.startify").config)
+        end
+    },
+
+    {"andweeb/presence.nvim"},
+    {"nvim-tree/nvim-web-devicons"},
+    {"lewis6991/gitsigns.nvim"},
+
+    {"neovim/nvim-lspconfig"},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"SirVer/ultisnips"},
+    {"quangnguyen30192/cmp-nvim-ultisnips"},
+
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        vim.fn["UltiSnips#Anon"](args.body)
+                    end
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered()
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true })
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'ultisnips' }
+                }, {
+                    { name = 'buffer' }
+                })
+            })
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+            require("lspconfig")["pyright"].setup {
+                capabilities = capabilities
             }
-        }
-    })
-
-    require("indent_blankline").setup({
-        show_current_context = true
-    })
-
-    require("barbar").setup({
-        animations = true
-    })
-
-    require("Comment").setup({
-        toggler = {
-            line = [[<C-_>]]
-        },
-        opleader = {
-            line = [[<C-_>]]
-        }
-    })
-
-    local cmp = require("cmp")
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                vim.fn["UltiSnips#Anon"](args.body)
-            end
-        },
-        window = {
-            completion = cmp.config.window.bordered(),
-            documentation = cmp.config.window.bordered()
-        },
-        mapping = cmp.mapping.preset.insert({
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<Tab>'] = cmp.mapping.confirm({ select = true })
-        }),
-        sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'ultisnips' }
-        }, {
-            { name = 'buffer' }
-	})
-    })
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-    require("lspconfig")["pyright"].setup {
-        capabilities = capabilities
-    }
-    require("lspconfig")["clangd"].setup {
-        capabilities = capabilities
-    }
-    require("lspconfig")["lua_ls"].setup {
-        capabilities = capabilities
+            require("lspconfig")["clangd"].setup {
+                capabilities = capabilities
+            }
+            require("lspconfig")["lua_ls"].setup {
+                capabilities = capabilities
+            }
+        end
     }
 
-end)
+})
